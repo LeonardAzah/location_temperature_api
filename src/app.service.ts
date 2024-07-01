29d945bf58fc73f38634b5f975242f5b2ba9dd5b
LeonardAzah2 <azahleonard@gmail.com>
2024-07-01 21:47:49 +0100

    accu app

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
 
 @Injectable()
@@ -6,38 +6,60 @@ export class AppService {
   private readonly ACCUWEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
   async getHello(visitorName: string, clientIp: string) {
-    const locationResponse = await axios.get(
-      `http://dataservice.accuweather.com/locations/v1/cities/ipaddress`,
-      {
-        params: {
-          apikey: this.ACCUWEATHER_API_KEY,
-          q: clientIp,
+    try {
+      const locationResponse = await axios.get(
+        `http://dataservice.accuweather.com/locations/v1/cities/ipaddress`,
+        {
+          params: {
+            apikey: this.ACCUWEATHER_API_KEY,
+            q: clientIp,
+          },
         },
-      },
-    );
-    if (locationResponse.status !== 200 || !locationResponse.data.EnglishName) {
-      throw new Error('Unable to fetch location data');
-    }
-    const location: string = locationResponse.data.EnglishName;
-    const locationKey: string = locationResponse.data.Key;
+      );
+      console.log(locationResponse);
 
-    const weatherData = await axios.get(
-      `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`,
-      {
-        params: {
-          apikey: this.ACCUWEATHER_API_KEY,
+      if (
+        locationResponse.status !== 200 ||
+        !locationResponse.data.EnglishName
+      ) {
+        throw new HttpException(
+          'Unable to fetch location data',
+          HttpStatus.SERVICE_UNAVAILABLE,
+        );
+      }
+      const location: string = locationResponse.data.EnglishName;
+      const locationKey: string = locationResponse.data.Key;
+
+      const weatherData = await axios.get(
+        `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}`,
+        {
+          params: {
+            apikey: this.ACCUWEATHER_API_KEY,
+          },
         },
-      },
-    );
-    if (weatherData.status !== 200) {
-      throw new Error('Unable to fetch weather data');
-    }
-    const temperature: number = weatherData.data[0].Temperature.Metric.Value;
+      );
+      if (weatherData.status !== 200) {
+        throw new HttpException(
+          'Unable to fetch weather data',
+          HttpStatus.SERVICE_UNAVAILABLE,
+        );
+      }
+      const temperature: number = weatherData.data[0].Temperature.Metric.Value;
 
-    return {
-      client_ip: clientIp,
-      location: location,
-      greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`,
-    };
+      return {
+        client_ip: clientIp,
+        location: location,
+        greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`,
+      };
+    } catch (error) {
+      console.error(
+        'Error:',
+        error.response ? error.response.data : error.message,
+      );
+      throw new HttpException(
+        'Error fetching data',
+        HttpStatus.SERVICE_UNAVAILABLE,
+      );
+    }
   }
 }
